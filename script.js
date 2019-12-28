ee46156b0690a90fd4c6332fef2c9f006112c05a
Kreavita <37907534+Kreavita@users.noreply.github.com>
2019-12-28 18:14:38 +0100

    Design changes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 window.onload=function () {
-    
+    fadeInfo("Hallo", "Test123");
+}
+//FadeDesign
+function fadeRemove(obj) {
+	$(obj).animate({opacity:0}, 200, function(){
+		obj.remove();
+	});
+}
+function fadeUpdate(obj, way, newobj){
+  $("#"+obj).animate({opacity:0}, 200, function(){
+  	if(way=="src"){
+  		document.getElementById(obj).src=newobj;
+	}
+  	if(way=="html"){
+  		document.getElementById(obj).innerHTML=newobj;
+	}
+  	if(way=="jquery"){
+  		$(obj).load(newobj);
+	}
+   $("#"+obj).animate({opacity:1}, 200);
+  });
+}
+function fadeInfo(title, content) {
+	var e=document.createElement("div");
+	e.classList.add("alertbox-container");
+	e.style.opacity=0;
+	e.innerHTML='<div class="alertbox"><div id="title">'+title+'</div><div id="content">'+content+'</div><div id="action"><div class="superbtn" onclick="fadeRemove(this.parentElement.parentElement.parentElement)">OK</div></div></div>'
+	document.body.append(e);
+	$(e).animate({opacity:1}, 200);
 }
